ba4c8bc5c3c391cdd98e7d674d56a42b0d3f4078
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-01-24 11:12:24 +0600

    Shifting array elements

---
+++ Array ADT/Shifting.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+    Author: Rakin
+    Date:  17-01-2022
+    Language: C
+    Content: Array ADT
+*/
+
+struct Array
+{
+    int A[20];
+    int size;
+    int length;
+};
+
+
+void left_shift(struct Array *arr)
+{
+    for(int i=0; i<arr->length-1; i++)
+        arr->A[i] = arr->A[i+1];
+    arr->A[arr->length-1] = 0;
+}
+
+
+void right_shift(struct Array *arr)
+{
+    for(int i=arr->length-1; i>0; i--)
+        arr->A[i] = arr->A[i-1];
+    arr->A[0] = 0;
+}
+
+
+void display_array(struct Array arr)
+{
+    printf("Printing all array elements(%d) : \n", arr.length);
+    for(int i=0; i<arr.length; i++)
+        printf("\t%d", arr.A[i]);
+    printf("\n\n");
+}
+
+
+int main()
+{
+    printf("\t***** An Example of Array ADT *****\n\n");
+
+    struct Array arr = {{10, 20, 30, 40, 50}, 20, 5};
+
+    display_array(arr);
+
+    printf("After shifting elements on left -> \n");
+    left_shift(&arr);
+    display_array(arr);
+
+    printf("After shifting elements on right -> \n");
+    right_shift(&arr);
+    display_array(arr);
+
+    return 0;
+}
