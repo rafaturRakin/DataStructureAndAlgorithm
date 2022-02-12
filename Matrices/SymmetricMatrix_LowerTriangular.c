0e13a837b6037204fcb5bef7550537b15f309c2b
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-02-12 22:58:32 +0600

    Symmetric matrix using lower triangular matrix

---
+++ Matrices/SymmetricMatrix_LowerTriangular.c
@@ -0,0 +1,97 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/*
+    Author: Rakin
+    Date:  12-02-2022
+    Language: C
+    Content:  Symmetric Matrix (Lower Triangular)
+*/
+
+struct Matrix
+{
+    int *A;
+    int dimension;
+};
+
+void set_value(struct Matrix *m, int i, int j, int value)
+{
+    int index;
+    if(i==0 || j==0 || i>m->dimension || j>m->dimension)
+        printf("Value not set!! Invalid Index (%d, %d)!!\n", i, j);
+    else if(i>=j)
+    {
+        index = i*(i-1)/2 + j-1;
+        m->A[index] = value;
+    }
+}
+
+
+int get_value(struct Matrix m, int i, int j)
+{
+    int index;
+    if(i==0 || j==0 || i>m.dimension || j>m.dimension)
+    {
+        printf("No value in this Invalid Index(%d, %d)!!\n", i, j);
+        return -1;
+    }
+    else if(i>=j)
+    {
+        index = i*(i-1)/2 + j-1;
+        return m.A[index];
+    }
+    else
+    {
+        index = j*(j-1)/2 + i-1;
+        return m.A[index];
+    }
+}
+
+
+void display_matrix(struct Matrix m)
+{
+    printf("\nPrinting (%d * %d) symmetric matrix using (lower triangular matrix) : \n", m.dimension, m.dimension);
+
+    for(int i=1; i<=m.dimension; i++)
+    {
+        printf("\t");
+        for(int j=1; j<=m.dimension; j++)
+            printf("%5d ", get_value(m, i, j));
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+int main()
+{
+    printf("\t***** An Example of Symmetric Matrix (Lower Triangular) in C *****\n\n");
+
+    struct Matrix m;
+    int value;
+
+    printf("Enter matrix dimension : ");
+    scanf("%d", &m.dimension);
+    m.A = (int *) malloc(sizeof(int) * m.dimension*(m.dimension+1)/2);
+
+    printf("Enter values : \n");
+    for(int i=1; i<=m.dimension; i++)
+    {
+        printf("\t");
+        for(int j=1; j<=m.dimension; j++)
+        {
+            scanf("%d", &value);
+            set_value(&m, i, j, value);
+        }
+    }
+
+    display_matrix(m);
+
+    printf("main() : Value in position[0,0] : %d\n\n", get_value(m, 0, 0));
+    printf("main() : Setting value in position[0,0] \n");
+    set_value(&m, 0, 0, 10);
+
+    free(m.A);
+    return 0;
+}
+
