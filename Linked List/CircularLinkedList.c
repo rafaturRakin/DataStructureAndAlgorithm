184947545a8447c7a01134713d7ad063f4beb91a
Rafatur Rahman Rakin <62703734+rafaturRakin@users.noreply.github.com>
2022-03-20 20:53:22 +0600

    Inserting in Circular Linked List

--- Linked List/CircularLinkedList.c
+++ Linked List/CircularLinkedList.c
@@ -14,6 +14,7 @@ struct Node
     struct Node* next;
 } *head;
 
+int list_length(struct Node *);
 // creating circular linked list
 void create_linkedList(int arr[], int len)
 {
@@ -38,7 +39,7 @@ void create_linkedList(int arr[], int len)
 // displaying linked list
 void display_linkedList(struct Node *p)
 {
-    printf("Displaying the circular linked list \n\t");
+    printf("Displaying the (%d node) circular linked list \n\t", list_length(p));
     do
     {
         printf("%4d  ", p->data);
@@ -59,14 +60,76 @@ void recursive_display(struct Node *h,int flag)
     }
 }
 
+int list_length(struct Node * h)
+{
+    int len = 0;
+    if(h==NULL)
+        return len;
+    do
+    {
+        ++len;
+        h = h->next;
+    }while(h!=head);
+    return len;
+}
+
+
+void insert_node(struct Node *h, int index, int value)
+{
+    struct Node *current_node, *temp_node;
+    if(index<0 || index>list_length(h))
+    {
+        printf("\tInvalid Index!!!%d is not inserted...\n", value);
+        return;
+    }
+    if(index==0)
+    {
+        temp_node = (struct Node *) malloc(sizeof(struct Node));
+        temp_node->data = value;
+        if(head==NULL)
+        {
+            printf("\t%d is now head and 1st node in the list.\n", value);
+            head = temp_node;
+            head->next = head;
+        }
+        else
+        {
+            while(h->next != head)
+                h = h->next;
+            h->next = temp_node;
+            temp_node->next = head;
+            head = temp_node;
+            printf("\tNew head is %d\n", head->data);
+        }
+    }
+    else
+    {
+        for(int i=0; i<index-1; i++)
+            h = h->next;
+        temp_node = (struct Node *) malloc(sizeof(struct Node));
+        temp_node->data = value;
+        temp_node->next = h->next;
+        h->next = temp_node;
+        printf("\t%d is inserted at #%d index\n", value, index);
+    }
+}
+
 int main()
 {
     printf("\t***** An Example of Circular Linked List in C *****\n\n");
 
     int arr[7] = {1, 2, 4, 8, 16, 32, 64};
     create_linkedList(arr, 7);
-    printf("Displaying recursively...\n");
-    recursive_display(head, 0);
+    printf("Displaying list...\n");
+    display_linkedList(head);
+
+    printf("\n");
+    insert_node(head, 0, 128);
+    insert_node(head, 3, 12);
+    insert_node(head, 10, 512);
+
+    printf("\n");
+    display_linkedList(head);
     return 0;
 }
 
